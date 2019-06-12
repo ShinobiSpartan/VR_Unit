fc2deec6978fac3c4ef57b8ee7cc944069af21c3
Nathan <njmeyer99@gmail.com>
2019-06-12 23:40:37 +1000

    Added Pathfinding Scene + started Pathfinding code

---
+++ VR_Project/Assets/Scripts/Node.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node
+{
+    public bool isWalkable;
+    public Vector3 worldPosition;
+
+    public Node(bool walkable, Vector3 worldPos)
+    {
+        isWalkable = walkable;
+        worldPosition = worldPos;
+    }
+}
