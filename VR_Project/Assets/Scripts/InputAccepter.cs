494d1ae6c0248ffeaf43e17868b7f83af12e3f88
Nathan <njmeyer99@gmail.com>
2019-06-14 15:24:39 +1000

    Added Oculus Utilities and Input Manager Scripts

---
+++ VR_Project/Assets/Scripts/InputAccepter.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputAccepter : MonoBehaviour
+{
+    private void Awake()
+    {
+        PlayerInput.onTriggerDown += TriggerDown;   
+    }
+
+    private void TriggerDown()
+    {
+
+    }
+
+    private void TriggerUp()
+    {
+
+    }
+}
