20d570068909b1666101a15250256fe8f6e63739
LeCarlo <liamr65@hotmail.com>
2019-06-20 10:26:20 +1000

    Added in a candle script

---
+++ VR_Project/Assets/Scripts/Candle.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Candle : MonoBehaviour
+{
+    public Light candle;
+    private void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.L))
+        { 
+            if (candle.enabled)
+            {
+                candle.enabled = false;
+            }
+            else
+            {
+                candle.enabled = true;
+            }
+        }
+    }
+}
